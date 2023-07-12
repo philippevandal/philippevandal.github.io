52c06f571d28c5940961bedbe972241d511b424a
philippevandal <34427212+philippevandal@users.noreply.github.com>
2023-07-12 16:28:07 -0400

    Add files via upload

---
+++ scripts/petri_3.py
@@ -0,0 +1,142 @@
+import cv2
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import numpy as np
+import RPi.GPIO as GPIO
+
+def detect_circles(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    blurred = cv2.GaussianBlur(image, (5, 5), 0)
+
+    # src = cv2.imread(img, cv2.IMREAD_COLOR)
+    # gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.medianBlur(gray, 5)
+    # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, min_dist,
+    #                           param1=edge_threshold, param2=centre_threshold,
+    #                           minRadius=min_radius, maxRadius=max_radius)
+
+    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1, minDist=min_dist,
+                               param1=edge_threshold, param2=centre_threshold, minRadius=min_radius, maxRadius=max_radius)
+
+    if circles is not None:
+        circles = circles.round() #.astype(int)
+
+    return circles
+
+def export_circles_to_csv(circles, iteration, csv_path):
+    if circles is None:
+        print("No circles detected.")
+        return
+
+    circle_data = []
+    for circle in circles[0]:
+        x, y, r = circle
+        circle_data.append({'x': x, 'y': y, 'radius': r, 'iteration': iteration})
+
+    df = pd.DataFrame(circle_data)
+
+    # Export the DataFrame to CSV
+    df.to_csv(csv_path, mode='a', index=False, header=not pd.read_csv(csv_path).empty)
+    print("Circle data exported to", csv_path)
+
+def plot_circle_data(csv_path):
+
+    df = pd.read_csv(csv_path)
+
+    iterations = df['iteration'].unique()
+    colors = plt.cm.rainbow(iterations / iterations.max())
+
+    for iteration, color in zip(iterations, colors):
+        iter_df = df[df['iteration'] == iteration]
+        plt.scatter(iter_df['x'], iter_df['y'], s=iter_df['radius']*10, alpha=0.5, color=color)
+
+    plt.axis('off')
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+def analyze_iterations(csv_path):
+    df = pd.read_csv(csv_path)
+
+    first_iteration_data = df[df['iteration'] == 1]
+    second_iteration_data = df[df['iteration'] == 2]
+    third_iteration_data = df[df['iteration'] == 3]
+
+    combined_data = pd.concat([first_iteration_data, second_iteration_data, third_iteration_data], ignore_index=True)
+
+    growth_factor = 0.1
+    combined_data['growth_radius'] = combined_data['radius'] * np.exp(growth_factor * combined_data['iteration'])
+
+    plt.scatter(combined_data['x'], combined_data['y'], s=combined_data['growth_radius']*10, alpha=0.5)
+    plt.axis('off')
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+
+def move_stepper_motor(dir, steps):
+
+    for i in range(steps):
+        for halfstep in range(8):
+            if dir == "r":
+                for pin in reversed(range(4)):
+                    GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+                    time.sleep(0.001)
+            else:
+                for pin in range(4):
+                    GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+                    time.sleep(0.001)
+    GPIO.cleanup()
+
+csv_path = 'circles.csv'
+iterations = 3
+
+cap = cv2.VideoCapture(0)
+min_dist = 10
+edge_threshold = 120
+centre_threshold = 20
+min_radius = 5
+max_radius = 40
+
+GPIO.setmode(GPIO.BOARD)
+control_pins = [7,11,13,15]
+positions = []
+
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
+
+halfstep_seq = [
+  [1,0,0,0],
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,0,0,1],
+  [1,0,0,1]
+]
+
+while True:
+    for iteration in range(0, iterations):
+        move_stepper_motor("a", positions[iteration])
+
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        circles = detect_circles(frame)
+
+        export_circles_to_csv(circles, iteration, csv_path)
+
+    move_stepper_motor("r", positions[-1])
+    GPIO.cleanup()
+
+    # plot_circle_data(csv_path)
+    analyze_iterations(csv_path)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
